f5e9b44ca3a8e40d735ebb290967ca752e691e17
Emiliano Massai <emilianomassai@icloud.com>
2020-11-20 15:11:28 +0000

    .

--- milestone_1_server.js
+++ milestone_1_server.js
@@ -6,6 +6,13 @@ app.set("port", process.env.PORT || 5000);
 
 app.get("/getHello", sayHello);
 
+// folder where all the static files live
+app.use(express.static("public"));
+
+// views is directory for all template files
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
 app.listen(app.get("port"), function () {
   console.log("Now listening for connections on port: ", app.get("port"));
 });
