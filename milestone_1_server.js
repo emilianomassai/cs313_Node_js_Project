5ab8d6e17b78632478bd18bc5bdd8a88faac812d
Emiliano Massai <emilianomassai@icloud.com>
2020-11-20 15:35:28 +0000

    .

--- milestone_1_server.js
+++ milestone_1_server.js
@@ -1,10 +1,6 @@
 var express = require("express");
 var app = express();
 
-// to use real time chat feature, passing the port we want to
-// use for socket.io
-var socket = require("socket.io")(process.env.PORT || 5000);
-
 // set the local and environment port to connect to
 app.set("port", process.env.PORT || 5000);
 
@@ -12,10 +8,6 @@ app.get("/getHello", sayHello);
 
 // folder where all the static files live
 app.use(express.static("public"));
-
-// views is directory for all template files
-app.set("views", __dirname + "/views");
-app.set("view engine", "ejs");
 
 app.listen(app.get("port"), function () {
   console.log("Now listening for connections on port: ", app.get("port"));
@@ -25,17 +17,3 @@ function sayHello(req, res) {
   console.log("Look at the browser window...");
   res.send("Hello World!");
 }
-
-// every time an user loads the server, the app will load this
-// function, giving to each user their own socket.
-socket.on(
-  "connection",
-  // then, we can send a message to the user, using this socket
-  (socket) => {
-    socket.emit(
-      "chat-message",
-      "Now listening for connections on port: ",
-      app.get("port")
-    );
-  }
-);
