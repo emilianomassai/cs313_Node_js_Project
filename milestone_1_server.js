58dd245df6e1483294fa3e8d834e85598a16fa70
Emiliano Massai <emilianomassai@icloud.com>
2020-11-20 17:55:19 +0000

    .

--- milestone_1_server.js
+++ milestone_1_server.js
@@ -15,15 +15,14 @@ const pool = new Pool({ connectionString: connectionString });
 app.set("port", process.env.PORT || 5000);
 
 app.get("/getUser", getUser);
-
 app.get("/sign_in", signIn);
 
-// // views is directory for all template files
-// app.set("views", __dirname + "/views");
-// app.set("view engine", "ejs");
+// views is directory for all template files
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-// // folder where all the static files live
-// app.use(express.static("public"));
+// folder where all the static files live
+app.use(express.static("public"));
 
 app.listen(app.get("port"), function () {
   console.log("Now listening for connections on port: ", app.get("port"));
@@ -42,8 +41,8 @@ function getUser(req, res) {
   console.log("Getting information from current user...");
 
   // to search for user by id, we need to do the following:
-  var id = req.query.chat_user.user_id;
-  console.log("Retrieving person with id: ", id);
+  var user_id = req.query.chat_user.user_id;
+  console.log("Retrieving person with id: ", user_id);
 
   // call the function passing the typed id and the function which displays
   // the result on the console
@@ -64,8 +63,8 @@ function getUser(req, res) {
  * it will display the related values as string.
  ******************************************************************************/
 // define the getPersonFromDb function used above
-function getUserFromDb(id, callback) {
-  console.log("getUserFromDb called with id: ", id);
+function getUserFromDb(user_id, callback) {
+  console.log("getUserFromDb called with id: ", user_id);
 
   // sequel, declaring that the passed id will be an integer and it will be
   // passed as first parameter
@@ -73,7 +72,7 @@ function getUserFromDb(id, callback) {
     "SELECT user_id, name_user, password, nickname FROM chat_user WHERE user_id = $1::int";
 
   // parameters saved as array (in this case we have only a value, id)
-  var params = [id];
+  var params = [user_id];
 
   // postgres module, please go and run this query (sql) with this parameters (params) and when is done call the callback function
   pool.query(sql, params, function (err, result) {
