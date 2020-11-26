1599e5bce149ad9b3b9a8073f94dcdd9ebf2740f
Emiliano Massai <emilianomassai@icloud.com>
2020-11-26 10:35:31 +0000

    week 11 development

---
+++ public/chat-clientside.js
@@ -0,0 +1,48 @@
+function searchUser() {
+  console.log("Searching user ...");
+
+  // the following is jQuery code to get the value from the text box (from
+  // scriptures.html page) and store the value into a local variable
+  var user_id = $("#user_id").val();
+  console.log("User id: " + user_id);
+
+  // 1. AJAX request to the server to search ///////////////////////////////////
+  // In the same way we can do with $post() to interact with the server/////////
+
+  // call the method search from index.js and look for a book
+  $.post("/getUser", { user_id: user_id }, function (data) {
+    console.log("Back from the server with: ");
+    console.log(data);
+
+    // 2. Getting the data back from the server ////////////////////////////////
+
+    // for loop to get elements of the list and take them out
+    // each of them as we go, to be able to display them into the html page
+    for (var i = 0; i < data.list.length; i++) {
+      var user = data.list[i];
+
+      // 3. Using the results to update the HTML page //////////////////////////
+
+      $("#resultFromServer").append(
+        "An user is found in our database with the following info: " +
+          "<br>" +
+          "<li>" +
+          "Username: " +
+          user.Name +
+          " " +
+          "Nickname: " +
+          user.Nickname +
+          " " +
+          "Password: " +
+          user.Password +
+          "</li>"
+      );
+      //       console.log(scripture);
+    }
+    //     // select the list on the scriptures.html page where to display the
+    //     // results
+    //     // $("#ulScriptures").append("<li>item1</li>");
+    //     // $("#ulScriptures").append("<li>item2</li>");
+    //     // $("#ulScriptures").append("<li>item3</li>");
+  });
+}
