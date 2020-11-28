721b915470721f576a91a71e3ab1cce5c94ccc8d
Emiliano Massai <emilianomassai@icloud.com>
2020-11-28 16:53:10 +0000

    .

--- milestone_1_server.js
+++ milestone_1_server.js
@@ -124,7 +124,7 @@ function checkForUser(req, res) {
 
       // res.status(500).json({ success: false, data: "No user found!" });
     } else {
-      res.json(result[0][0]);
+      res.json(result[0][user_id]);
     }
   });
 }
