15fcb56ac50bef2c134d08c6e2f7fc9e71d8d02e
Emiliano Massai <emilianomassai@icloud.com>
2020-11-28 20:23:27 +0000

    .

--- milestone_1_server.js
+++ milestone_1_server.js
@@ -200,12 +200,13 @@ function checkForUserFromDb(name_user, password, callback) {
 }
 
 function addMessageToDB(message_user_id, message_text) {
-  var id_user = 1;
-  var message = "From addMessageToDB local var the message is saved!";
+  // var id_user = 1;
+  // var message = "From addMessageToDB local var the message is saved!";
 
   var sql =
     "INSERT INTO chat_message(message_user_id, message_text) VALUES($1, $2)";
-  var params = [id_user, message];
+  // var params = [id_user, message];
+  var params = [message_user_id, message_text];
 
   pool.query(sql, params, function (err, result) {
     if (err) {
